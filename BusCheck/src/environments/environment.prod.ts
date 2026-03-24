caf0e668a27a111df22ecb1769cf623d1aa9d00d
Geovana Moura <mrsgeovanamoura@gmail.com>
2026-03-24 19:48:39 -0300

    Depuração Android

--- BusCheck/src/environments/environment.prod.ts
+++ BusCheck/src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  supabaseUrl: 'https://gnisqzgjazqezgsxyddx.supabase.co',
+  supabaseKey: 'sb_publishable_OEArED_rLAmiRUH3lVq3yg_D_8HBBB9'
 };
---
+++ android/app/src/main/java/com/buscheck/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.buscheck.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
