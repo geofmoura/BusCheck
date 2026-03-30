b5552bab1eda2e291049f7ef68b019e2b28edb57
Geovana Moura <mrsgeovanamoura@gmail.com>
2026-03-30 00:43:56 -0300

    refactor(login): reformula tela de login para padrão consistente
    - Remove componentes do Material Design
    - Implementa formulário reativo com validações
    - Adiciona campo de senha com toggle de visibilidade
    - Padroniza estilo visual com tela de cadastro
    - Adiciona opção 'lembrar-me' e 'esqueci senha'
    - Melhora responsividade da interface

--- BusCheck/src/app/home/home.module.ts
+++ BusCheck/src/app/home/home.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
-import { FormsModule } from '@angular/forms';
 import { HomePage } from './home.page';
-
 import { HomePageRoutingModule } from './home-routing.module';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {Component, signal} from '@angular/core';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
 
 
 @NgModule({
@@ -12,7 +16,8 @@ import { HomePageRoutingModule } from './home-routing.module';
     CommonModule,
     FormsModule,
     IonicModule,
-    HomePageRoutingModule
+    HomePageRoutingModule,
+    MatFormFieldModule, MatInputModule, FormsModule, ReactiveFormsModule, MatButtonModule, MatIconModule
   ],
   declarations: [HomePage]
 })
